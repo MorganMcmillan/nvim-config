b685ad550637154018cfce12b3b9fa8b31173961
morganmcmillan <mcmillan.a.morgan@gmail.com>
2026-06-12 21:54:46 -0600

    Create configuration

---
+++ lua/plugins/spec1.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {
+            default_file_explorer = true,
+            buf_options = {
+                buflisted = false,
+                bufhidden = "hide",
+            },
+        },
+        -- Optional dependencies
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        skip_confirm_for_simple_edits = true,
+    }
+}
