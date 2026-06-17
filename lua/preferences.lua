3061b41ca7bd664c76d213c8fba4851bc5f55000
morganmcmillan <mcmillan.a.morgan@gmail.com>
2026-06-17 13:59:59 -0600

    Add key maps

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -1,3 +1,17 @@
-vim.cmd"set tabstop=4"
-vim.cmd"set shiftwidth=4"
-vim.cmd"set expandtab"
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+
+local function setkey(key, command, mode)
+  mode = mode or "n"
+  vim.keymap.set(mode, key, command, { noremap = true })
+end
+
+setkey("{", "{zz")
+setkey("}", "}zz")
+setkey("*", "*zz")
+setkey("#", "#zz")
+setkey("n", "nzz")
+setkey("N", "Nzz")
+
+setkey("ZX", "<cmd>qa!<CR>")
